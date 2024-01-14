5c71371a6b50015b98d4e0f0b7d356d5b0af40fc
nicosmico <niccolas.dev@gmail.com>
2024-01-14 13:54:35 -0300

    feat: Install Tailwind and Prettier

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  trailingComma: 'es5',
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className='text-5xl text-orange-600'>Dividy</h1>
+    </>
+  );
+}
+
+export default App;
