246b4ad1f9c7f3ec58344355033306e0d9230f15
nicosmico <niccolas.dev@gmail.com>
2024-02-17 19:31:13 -0300

    feat(Select): Add custom select

---
+++ src/components/form/Select.tsx
@@ -0,0 +1,49 @@
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
+
+const arrowIcon =
+  'bg-[url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAUCAMAAACtdX32AAAAdVBMVEUAAAD///8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAhMdQaAAAAJ3RSTlMAAAECAwQGBwsOFBwkJTg5RUZ4eYCHkJefpaytrsXGy8zW3+Do8vNn0bsyAAAAYElEQVR42tXROwJDQAAA0Ymw1p9kiT+L5P5HVEi3qJn2lcPjtIuzUIJ/rhIGy762N3XaThqMN1ZPALsZPEzG1x8LrFL77DHBnEMxBewz0fJ6LyFHTPL7xhwzWYrJ9z22AqmQBV757MHfAAAAAElFTkSuQmCC)]';
+
+interface Props extends React.SelectHTMLAttributes<HTMLSelectElement> {
+  label: string;
+  register?: UseFormRegisterReturn<string>; // For react-hook-form
+}
+export function Select({
+  label,
+  register,
+  children,
+  className,
+  ...props
+}: Props) {
+  const randomNumber = Math.random().toString().replace('.', '').slice(1, 5);
+  const id = register?.name ? register.name + randomNumber : props.id;
+
+  return (
+    <div className='relative w-full'>
+      <select
+        id={id}
+        {...props}
+        {...register}
+        className={twMerge(
+          'peer block w-full appearance-none rounded-xl border-transparent p-4 outline-zinc-800 focus:border-zinc-800 focus:ring-zinc-800 disabled:pointer-events-none disabled:opacity-50 has-[option:checked]:pb-2 has-[option:checked]:pt-6',
+          'appearance-none bg-right bg-no-repeat',
+          arrowIcon,
+          className
+        )}
+      >
+        {children}
+      </select>
+      <label
+        htmlFor={id}
+        className={twMerge(
+          'text-md pointer-events-none absolute start-0 top-0 h-full truncate border border-transparent p-4 transition duration-100 ease-in-out',
+          'peer-has-[option:checked]:-translate-y-2.5 peer-has-[option:checked]:text-xs peer-has-[option:checked]:text-gray-700'
+        )}
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default Select;
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,3 +1,5 @@
+import { Select } from '../form/Select';
 import { Input } from './Input';
 import { InputError } from './InputError';
-export { Input, InputError };
+
+export { Input, InputError, Select };
