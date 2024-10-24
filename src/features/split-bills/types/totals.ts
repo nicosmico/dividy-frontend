ecea36bf89ec2f0f01bd7b30e18ed62057598e78
nicosmico <niccolas.dev@gmail.com>
2024-10-24 19:18:48 -0300

    feat: Add totals page

--- src/features/split-bills/types/totals.ts
+++ src/features/split-bills/types/totals.ts
@@ -1,66 +1,76 @@
-import { Bill, BillItem } from './bill';
+import { Bill } from './bill';
+import { Member } from './member';
 
-export interface MemberSummary {
-  id: string;
-  member: string; // Member ID
-  debts: Debt[];
-  incomes: Debt[];
+export interface MemberDebtsSummary {
+  debtor: Member;
+  debts: MemberDebt[];
+  total: number;
 }
 
-export interface Debt {
-  id: string;
-  debtor: string; // Member ID
-  bill: Bill; // Bill detail (includes the paidBy field)
-  items: BillItem[]; // Items that debtor must pay
-  total: number; // Total amount of items
+export interface MemberDebt {
+  payer: Member;
+  bills: MemberDebtBill[];
+  total: number;
+}
+
+export interface MemberDebtBill {
+  bill: Bill; // Original bill
+  items: MemberDebtBillItem[];
+  total: number;
+}
+
+export interface MemberDebtBillItem {
+  name: string;
+  price: number;
+  totalByMember: number;
+  members: number;
 }
 
 export class Summary {
-  constructor(private readonly bills: Bill[]) {}
+  constructor(
+    private readonly bills: { [id: string]: Bill },
+    private readonly billsOrder: string[],
+    private readonly members: { [id: string]: Member },
+    private readonly membersOrder: string[]
+  ) {}
 
-  private getMembersDebts(): Record<string, Debt[]> {
-    // Group bill to pay by member
-    // const billsToPayByMember: { [memberUuid: string]: Debt[] } = {};
-    const indexedDebts: Record<string, Record<string, Debt>> = {};
-    this.bills.forEach((bill) => {
+  public getMembersDebtSummary() {
+    const memberDebtsSummary: { [member: string]: MemberDebtsSummary } = {};
+
+    const bills = Object.values(this.bills);
+    bills.forEach((bill) => {
+      //
+
       bill.items.forEach((item) => {
-        item.members.forEach((memberUuid) => {
-          // Add Debt if not exists
-          indexedDebts[memberUuid] ??= {};
-          indexedDebts[memberUuid][bill.id] ??= {
-            id: crypto.randomUUID(),
-            debtor: memberUuid,
-            bill,
-            items: [],
-            total: 0,
-          };
+        //
 
-          // Add item to bill and update totalToPay
-          indexedDebts[memberUuid][bill.id].items.push(item);
-          indexedDebts[memberUuid][bill.id].total +=
-            item.price / item.members.length;
+        item.members.forEach((member) => {
+          // Check for existing member debt summary
+          if (!memberDebtsSummary[member]) {
+            memberDebtsSummary[member] = {
+              debtor: this.members[member],
+              debts: [],
+              total: 0
+            };
+          }
+
+          // Check for existing member to member summary
+          if (!memberDebtsSummary[member].debts.find(debt => debt.payer.id === bill.paidBy)) {
+            memberDebtsSummary[member].debts.push({
+              payer: this.members[bill.paidBy],
+              bills: [],
+              total: 0
+            });
+          }
+
+          const memberDebt: MemberDebt = {
+            payer: this.members[bill.paidBy],
+            bills: 
+          }
         });
       });
     });
 
-    // Format to Record<string, Debt[]>
-    const debts: Record<string, Debt[]> = {};
-    Object.entries(indexedDebts).forEach(([member, memberDebts]) => {
-      debts[member] = Object.values(memberDebts);
-    });
-    return debts;
-  }
-
-  private getMembersIncomes(): Record<string, Debt[]> {
-    this.bills.forEach((bill) => {
-      // Repensar como hacer todo el reusmen, no solo esto :(
-    });
-    return {};
-  }
-
-  // TODO: Return the member debts and incomes
-  public getMemberSummary() {
-    const memberDebts = this.getMembersDebts();
-    const memberIncomes = {};
+    return Object.values(memberDebtsSummary);
   }
 }
--- src/pages/split-bills/total/TotalsPage.tsx
+++ src/pages/split-bills/total/TotalsPage.tsx
@@ -1,19 +1,7 @@
 import { IconArrowNarrowLeft } from '@tabler/icons-react';
-import { useEffect } from 'react';
 import { RoundedLink } from 'src/components/ui';
-import useBills from 'src/features/split-bills/hooks/useBills';
-import useMembers from 'src/features/split-bills/hooks/useMembers';
-import { Summary } from 'src/features/split-bills/types/totals';
 
 export function TotalsPage() {
-  const { members } = useMembers();
-  const { bills } = useBills();
-
-  useEffect(() => {
-    const summary = new Summary(Object.values(bills));
-    // Get summary here
-  }, [bills]);
-
   return (
     <>
       <div className='grid grid-cols-1 gap-3 md:grid-cols-2 md:grid-rows-[min-content_1fr]'>
@@ -24,18 +12,7 @@ export function TotalsPage() {
             miembro del grupo.
           </p>
         </div>
-        <div className='space-y-8 md:col-start-2 md:col-end-3 md:row-span-full'>
-          {/* <ul className='w-full space-y-4'>
-          {membersDebts.map((memberDebts) => (
-            <li key={memberDebts.id}>
-              <MemberDebtsCard
-                memberDebts={memberDebts}
-                members={members}
-              ></MemberDebtsCard>
-            </li>
-          ))}
-        </ul> */}
-        </div>
+        <div className='space-y-8 md:col-start-2 md:col-end-3 md:row-span-full'></div>
         <div className='sticky bottom-2 md:static md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-3 md:content-start'>
           <RoundedLink
             to='../bills'
