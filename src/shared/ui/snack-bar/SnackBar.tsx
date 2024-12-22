8b3e54df7bbea33ad8db7c5be923b78af3bf6b3d
nicosmico <niccolas.dev@gmail.com>
2024-12-22 15:37:02 -0300

    fix(SnackBar): Fix font size

--- src/shared/ui/snack-bar/SnackBar.tsx
+++ src/shared/ui/snack-bar/SnackBar.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 export function SnackBar({ message, onClose }: Props) {
   return (
-    <Card className='flex items-center justify-between gap-2 bg-zinc-800 p-4 text-gray-50'>
-      <div className='text-balance'>{message}</div>
+    <Card className='flex items-center justify-between gap-2.5 bg-zinc-800 p-4 text-gray-50'>
+      <div className='text-balance text-sm'>{message}</div>
       <RoundedButton
-        className='bg-gray-50 px-3 py-1 text-zinc-800'
+        className='bg-gray-50 px-3 py-1 text-sm text-zinc-800'
         onClick={onClose}
       >
         Cerrar
